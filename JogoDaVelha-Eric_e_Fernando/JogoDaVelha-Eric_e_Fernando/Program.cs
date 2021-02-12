091adf450b3d7c79de6edfa4b7c784e2d309ab77
ericserra <78964631+ericserra@users.noreply.github.com>
2021-02-12 13:46:21 -0300

    Teste

--- JogoDaVelha-Eric_e_Fernando/JogoDaVelha-Eric_e_Fernando/Program.cs
+++ JogoDaVelha-Eric_e_Fernando/JogoDaVelha-Eric_e_Fernando/Program.cs
@@ -11,6 +11,23 @@ namespace JogoDaVelha_Eric_e_Fernando
         static void Main(string[] args)
         {
             string[,] posicoes = new string[3, 3];
+            for (int i = 0; i < posicoes.GetLength(0); i++)
+            {
+                for (int j = 0; j < posicoes.GetLength(1); j++)
+                {
+                    Console.WriteLine("[" + i + "," + j + "]");
+                    Console.ReadLine();
+                }
+            }
+            for (int i = 0; i < posicoes.GetLength(0); i++)
+            {
+                for (int j = 0; j < posicoes.GetLength(1); j++)
+                {
+                    Console.Write(posicoes[i,j]+"\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
         }
     }
 }
