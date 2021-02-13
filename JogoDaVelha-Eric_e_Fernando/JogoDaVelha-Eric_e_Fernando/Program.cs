0fb482bff8fb8cdf052b699b491778c6f3518318
ericserra <78964631+ericserra@users.noreply.github.com>
2021-02-13 17:30:12 -0300

    proximo commit

--- JogoDaVelha-Eric_e_Fernando/JogoDaVelha-Eric_e_Fernando/Program.cs
+++ JogoDaVelha-Eric_e_Fernando/JogoDaVelha-Eric_e_Fernando/Program.cs
@@ -12,25 +12,27 @@ namespace JogoDaVelha_Eric_e_Fernando
         {
             string[,] matriz = new string[3, 3];
             int[,] matrizInteira = new int[3, 3];
-            matrizInteira[0, 0] = 1;
-            matrizInteira[0, 1] = 2;
-            matrizInteira[0, 2] = 3;
-            matrizInteira[1, 0] = 4;
-            matrizInteira[1, 1] = 5;
-            matrizInteira[1, 2] = 6;
-            matrizInteira[2, 0] = 7;
-            matrizInteira[2, 1] = 8;
-            matrizInteira[2, 2] = 9;
+            matriz[0, 0] = "1";
+            matriz[0, 1] = "2";
+            matriz[0, 2] = "3";
+            matriz[1, 0] = "4";
+            matriz[1, 1] = "5";
+            matriz[1, 2] = "6";
+            matriz[2, 0] = "7";
+            matriz[2, 1] = "8";
+            matriz[2, 2] = "9";
             Console.WriteLine("Escolha uma das posições para começar a jogar\n");
             Imprimir_logo(matriz, matrizInteira);
             Console.ReadKey();
+            
         }
 
         static void Imprimir_logo(string[,] matriz, int[,] matrizInteira)
         {
-            Console.WriteLine("_"+matrizInteira[0,0]+"_||_"+matrizInteira[0,1]+"_||_"+matrizInteira[0,2]+"_");
-            Console.WriteLine("_"+matrizInteira[1,0]+"_||_"+matrizInteira[1,1]+"_||_"+matrizInteira[1,2]+"_");
-            Console.WriteLine("_"+matrizInteira[2,0]+"_||_"+matrizInteira[2,1]+"_||_"+matrizInteira[2,2]+"_");
+            
+            Console.WriteLine("_"+matriz[0,0]+"_||_"+matriz[0,1]+"_||_"+matriz[0,2]+"_");
+            Console.WriteLine("_"+matriz[1,0]+"_||_"+matriz[1,1]+"_||_"+matriz[1,2]+"_");
+            Console.WriteLine("_"+matriz[2,0]+"_||_"+matriz[2,1]+"_||_"+matriz[2,2]+"_");
         }
         static void verificarPosicoes(int[,] inteira, string[,] matriz, ref int jogada, int jogador )
         {
